78a10e7c5104c5cbe11f3ac410d92bae69c76340
SanaNasar <sanasehereen.cse@gmail.com>
2015-02-06 15:18:31 -0800

    Making some changes to Isomorph react

--- isomorphic-react/initialize.js
+++ isomorphic-react/initialize.js
@@ -1,5 +1,5 @@
-var React = require("react");
-var Favorites = require('./Favorites.jsx');
+var React = require("react"); // require React 
+var Favorites = require('./Favorites.jsx'); // require Favorites
 
 
 var App = require('./components/App.jsx');
@@ -10,6 +10,6 @@ var App = require('./components/App.jsx');
         <Favorites favorites={this.props.favorites} />
       </div>
     )
-  }
+  };
 
 React.render(App(), document.body);
